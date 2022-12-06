3aae681ec0fc843e0e36dd9c480a586e560d906c
VincentFerrigan <vincent.ferrigan@gmail.com>
2022-12-06 13:18:20 +0100

    separating the view into several components

--- src/pages/gameplay/Question.js
+++ src/pages/gameplay/Question.js
@@ -4,55 +4,40 @@ import './quotebox.css';
 function Question(props) {
 
     function alternativeCB(movie) {
-        return (<div key={movie.getId()}>
+        const id = movie.getId()
+        const title = movie.getTitle()
+        return (<div key={id}>
             <input type="radio"
-                   id={movie.getId()}
-                   value={movie.getId()}
+                   id={id}
+                   value={id}
                    name={"answer"}
-                   key={movie.getId()}
-                   onInput={() => {
-                       console.log(movie)
-                       props.onSelect(movie)
-                   }}
+                   key={id}
+                   onInput={() => {props.onSelect(id)}}
             />
-            <label htmlFor={movie.getId()}>{movie.getTitle()}</label>
+            <label htmlFor={id}>{title}</label>
         </div>);
     }
     function handleAnswerACB() {
         props.onSubmit()
     }
-    function nextQuoteRequestACB() {props.onNext(props.movieToQuote)}
+    // function nextQuoteRequestACB() {props.onNext(props.movieToQuote)}
+    function nextQuoteRequestACB() {props.onNext()}
     function characterRequestACB() {props.onCharacter()}
     function yearRequestACB() {props.onYear()}
 
     return (
         <>
-            {props.movieToQuote.getLines().split ('\n').map((item, i) => <p key={i}>{item}</p>)}
-            {props.isHintCharacter
-            ? props.movieToQuote.getCharacters().split ('\n').map((item, i) => <p key={i}>{item}</p>)
-            : null}
-            {props.isHintYear
-                ? `${props.movieToQuote.getYear()}`
-                : null}
             <form>
                 <fieldset>
                     <legend>Which movie?</legend>
                     {props.movies.map(alternativeCB)}
-                    <button onClick={handleAnswerACB} type={"submit"}>Submit</button>
                 </fieldset>
             </form>
+            <button onClick={handleAnswerACB} type={"submit"} disabled={props.hasSelected === ""}>Submit</button>
             <button onClick={nextQuoteRequestACB}>NextQuote</button>
             <button onClick={characterRequestACB}>Character hint</button>
             <button onClick={yearRequestACB}>Year hint</button>
-            <div>
-            {props.hasSubmittedAnswer
-                ? props.isAnswerCorrect
-                    ? "Congratulation!"
-                    : "You Lose! Good Day Sir!"
-                : null
-            }
-            </div>
         </>);
 }
 
-export default Question;
+export default React.memo(Question);
--- src/presenters/GamePresenter.js
+++ src/presenters/GamePresenter.js
@@ -4,16 +4,20 @@
  */
 import React, {useState} from 'react';
 import Question from "../pages/gameplay/Question";
-import {fetchMovieQuotes} from "../movieSource";
+import {fetchAllMoviesQ, fetchMovieQ} from "../movieSource";
 import {createQuoteGeneratorStatic} from "../utilities";
 import {QUOTE, QUOTE2, QUOTE3} from "../filmConsts";
+import QuoteBox from "../pages/gameplay/QuoteBox";
+import ResultBox from "../pages/gameplay/ResultBox";
 
 function GamePresenter(props) {
-    const [answer, setAnswer] = React.useState({})
+    const [answerId, setAnswerId] = React.useState({})
     const [hasSubmittedAnswer, setHasSubmittedAnswer] = React.useState(false)
     const [isLoading, setIsLoading] = React.useState(false)
+    const [error, setError] = React.useState(null)
 
     // switch these three movies to an array of objects instead
+    const [movies, setMovies] = React.useState([])
     const [movie1, setMovie1] = React.useState(null)
     const [movie2, setMovie2] = React.useState(null)
     const [movie3, setMovie3] = React.useState(null)
@@ -23,72 +27,84 @@ function GamePresenter(props) {
     const [showYear, setShowYear] = React.useState(false)
     const [isAnswerCorrect, setIsAnswerCorrect] = React.useState(false)
 
+    const addMovieHandler = (movie) => {
+        // setMovies(prevMovies => [...prevMovies, movie])
+        setMovies(prevMovies => [...prevMovies, createQuoteGeneratorStatic(movie)])
+    }
     // const fetchMovieQuotesHandler = async () => {
     const fetchMovieQuotesHandler = React.useCallback( async () => {
-            setIsLoading(true)
-            try {
-                if (!movie1 || !movie2 || !movie3) {
-                    /*
-                    let moviePromises = [
-                        fetchMovieQuotes('tt0068646'),
-                        fetchMovieQuotes('tt0073195'),
-                        fetchMovieQuotes('tt0073195'),
-                    ]
-                    Promise.all(moviePromises).then(movies => {
-                        // do something
-                    })
-                     */
+        setIsLoading(true)
+        setError(null)
+        setAnswerId("")
+        debugger; // this function is called twice when reloaded.
+        try {
+            /*
+            let moviePromises = [
+            fetchMovieQuotes('tt0068646'),
+            fetchMovieQuotes('tt0073195'),
+            fetchMovieQuotes('tt0073195'),
+            ]
+            Promise.all(moviePromises).then(movies => {
+            // do something
+            })
+            */
 
-                    /*
-                    const data = await fetchMovieQuotes('tt0068646')
-                    const data = await fetchMovieQuotes('tt0073195')
-                     */
+            /*
+            const data = await fetchMovieQ('tt0068646')
+            const data2 = await fetchMovieQ('tt0073195')
+            const data3 = await fetchMovieQ('tt0073195')
+            */
 
-                    // switch to a parallel fetch instead
-                    const data = await QUOTE
-                    const data2 = await QUOTE2
-                    const data3 = await QUOTE3
 
-                    setMovie1(createQuoteGeneratorStatic(data))
-                    setMovie2(createQuoteGeneratorStatic(data2))
-                    setMovie3(createQuoteGeneratorStatic(data3))
+            /*
+             * fetch a list of Genre,
+             * ask which genre the user wants to play with
+             * fetch list of movies
+             * choose three of them per game (random?)
+             * fetch quoteObjects
+             * create QuoteGenerators
+             */
+            // await setMovies(fetchAllMoviesQ(''))
+            await setMovies(QUOTE, QUOTE2, QUOTE3)
 
-                    setCorrectMovieId(data2.id)
-                }
-                // else {
-                // //     debugger;
-                // }
-            } catch(err) {
-                console.log(err)
-                debugger;
-            }
-            setIsLoading(false)
-        debugger;
-        }, [])
+            // switch to a parallel fetch instead
+            const data = await QUOTE
+            const data2 = await QUOTE2
+            const data3 = await QUOTE3
+
+            setMovie1(createQuoteGeneratorStatic(data))
+            setMovie2(createQuoteGeneratorStatic(data2))
+            setMovie3(createQuoteGeneratorStatic(data3))
+
+            setCorrectMovieId(data2.id)
+        } catch(err) {
+            setError(err.message)
+            console.error(err)
+            debugger;
+        }
+        setIsLoading(false)
+    }, [])
     // }
 
     React.useEffect(() => {
         console.log("Effect running ")
-        fetchMovieQuotesHandler()
+        fetchMovieQuotesHandler();
         return () => {console.log("Effect clean up")}
-    // }, [fetchMovieQuotesHandler])
-    }, [])
+    }, [fetchMovieQuotesHandler])
 
     function checkAnswerCB() {
-        console.log(answer.getId() === correctMovieId)
-        return answer.getId() === correctMovieId
-        debugger
+        return answerId === correctMovieId
     }
     function submitAnswerACB() {
         setHasSubmittedAnswer(true)
         setIsAnswerCorrect(checkAnswerCB())
     }
-    function selectedAnswerACB(movie) {
-        setAnswer(movie)
+    function selectedAnswerACB(id) {
+        setAnswerId(id)
     }
-    function nextQuoteACB(nextQuote){
-        nextQuote.popQuote()
-        setMovie2( {...nextQuote})
+    function nextQuoteACB(){
+        movie2.popQuote()
+        setMovie2( {...movie2})
         setShowCharacter(false)
         setShowYear(false)
     }
@@ -102,20 +118,27 @@ function GamePresenter(props) {
         <>
             {!isLoading && movie1 && (
                 <div>
-                <Question
+                <QuoteBox
                     movieToQuote = {movie2}
                     isHintCharacter = {showCharacter}
                     isHintYear = {showYear}
-                    isAnswerCorrect = {isAnswerCorrect}
-                    hasSubmittedAnswer = {hasSubmittedAnswer}
+                />
+                <Question
                     onSubmit={submitAnswerACB}
                     onNext={nextQuoteACB}
                     onCharacter={characterACB}
                     onYear={yearACB}
                     onSelect={selectedAnswerACB}
                     movies={[movie1, movie2, movie3]}
+                    hasSelected={answerId}
+                /></div>
+                )
+            }
+            {hasSubmittedAnswer && (
+                <ResultBox
+                isAnswerCorrect = {isAnswerCorrect}
                 />
-        </div>)}
+            )}
         </>
     );
 }
