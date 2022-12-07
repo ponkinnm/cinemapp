c60eca1b6e001bc06c384dd52e43bd3a43597d39
VincentFerrigan <vincent.ferrigan@gmail.com>
2022-12-07 01:17:47 +0100

    minor bug-fix

--- src/pages/gameplay/QuoteBox.js
+++ src/pages/gameplay/QuoteBox.js
@@ -5,16 +5,16 @@ function QuoteBox(props) {
     return (
         <>
             <b>The Quote:</b>
-            <p>
             {props.movieToQuote.getLines().split ('\n').map((item, i) => <p key={i}>{item}</p>)}
             {(props.isHintCharacter || props.isHintYear) ? (<b>Hint:</b>) : null}
-            </p><p>
+            <p>
             {props.isHintCharacter
             ? `- We quoted ${props.movieToQuote.getArrayOfCharacters()
                 .reduce((text, value, i, array) => 
                     text + (i < array.length - 1 ? ', ' : ' and ') + value)}.`
             : null}
-            </p><p>
+            </p>
+            <p>
             {props.isHintYear
                 ? `- The movie was made ${props.movieToQuote.getYear()}.`
                 : null}
