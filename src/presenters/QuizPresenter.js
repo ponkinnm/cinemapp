e519fc6f9a008586df79147dff3ae365f874137e
Syncopated90 <ehne@kth.se>
2022-12-14 09:35:41 +0100

    started a presenter layer above GamePresenter to make a genre chooser

---
+++ src/presenters/QuizPresenter.js
@@ -0,0 +1,8 @@
+import GamePresenter from './GamePresenter'
+import GetTitlesFromGenre from '../GetTitlesFromGenre'
+import {useState, useEffect} from 'react'
+
+export default function QuizPresenter(){
+    const [genre, setGenre] = React.useState('')
+
+}
