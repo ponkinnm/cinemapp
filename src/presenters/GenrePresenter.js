29ccddab36834284552047ea01829ab4ab8e1f27
VincentFerrigan <vincent.ferrigan@gmail.com>
2022-12-17 21:37:55 +0100

    Connecting GenrePresenter to RTK

--- src/presenters/GenrePresenter.js
+++ src/presenters/GenrePresenter.js
@@ -6,53 +6,52 @@ import {useGetMovieIdsByGenreQuery} from "../features/api/apiSlice";
 import {skipToken} from "@reduxjs/toolkit/query";
 import {gameSliceAction, replaceMovieIds, replaceTitleIdList} from "../features/game/gameSlice";
 import {useDispatch} from "react-redux";
-import LoadingScreen from "../LoadingScreen";
+import LoadingScreen from "../views/LoadingScreen";
+import {fetchTitleIdsByGenre} from "../features/game/gameApiActions";
+import GamePresenter from "./GamePresenter";
 
-export default function GenrePresenter(){
+export default function GenrePresenter() {
     const LIMIT = 100
     const [myGenre, setMyGenre] = useState(skipToken)
-    const dispatch = useDispatch()
-    // let content
 
     // alt 1
-    function handleGenreChange(event){
+    function handleGenreChange(event) {
         setMyGenre({
-            limit:LIMIT,
+            limit: LIMIT,
             genre: event.target.value
         })
     }
-    // alt 2
-    // function handleGenreChange(selectedGenre){
-    //     setGenre(selectedGenre)
-    // }
+
     const {
-        data,
         isLoading,
         isFetching,
         isSuccess,
         isError,
         error,
-        } = useGetMovieIdsByGenreQuery(myGenre)
-        // } =useGetMovieIdsByGenreQuery({limit:LIMIT, genre})
+    } = useGetMovieIdsByGenreQuery(myGenre)
 
     useEffect(() => {
-        console.log("Effect running genre set up ")
-        if (data) {
-            dispatch(gameSliceAction.replaceListOfMovieIds(data))
-        }
-        return () => {console.log("Effect clean up genre set up")}
-        }, [data]
+            console.log("Effect running genre set up ")
+            return () => {
+                console.log("Effect clean up genre set up")
+            }
+        }, []
     )
 
+    // TODO: create an error component?  Like LoadingScreen...
     return (
         <>
-            {myGenre === skipToken && <GenrePickerView
-                onGenre={handleGenreChange}
-                genres={GENRE}
-            />}
-            {isLoading && <LoadingScreen/>}
-            {error && <div>{`Houston, we have a problem! Tell the newbies that the ${error.message.toString()}`}</div>}
-            {isSuccess && <QuotePresenter/>}
+            {isError
+                ? <div>{`Houston, we have a problem! Tell the newbies that the ${error.message.toString()}`}</div>
+                : (isLoading || isFetching)
+                    ? <LoadingScreen/>
+                    : isSuccess
+                        ? <GamePresenter/>
+                        : <GenrePickerView
+                            setGenre={handleGenreChange}
+                            genres={GENRE}
+                        />
+            }
         </>
     );
 }
